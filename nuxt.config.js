f2ef81128118b316c9d56cdd00e393a329f609e5
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-05-10 14:05:17 +0900

    ツールバーおよびTOPページレイアウト

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: "yuuu's portfolio",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
